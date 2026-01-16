b4769393fa471724190c2961cc0be560222ef4cc
Wesly Cagua <weslykgua@gmail.com>
2026-01-16 17:13:13 -0300

    Remove unused components and configuration files from mobile app

--- mobile-app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.forgy.app',
-  appName: 'mobile-app',
-  webDir: 'dist'
-};
-
-export default config;
